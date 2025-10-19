9278cec7985374d9227da7e546bfca58f3134fa3
Ferdinand Thiessen <opensource@fthiessen.de>
2025-10-19 18:36:22 +0200

    chore: split frontend building into legacy Vue 2 and Vue 3
    - Built the frontend in separate packages until we migrated everything
      to Vue 3.
    - Separate logic into two packages controlled by main package.json
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+/*!
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+import { createAppConfig } from '@nextcloud/vite-config'
+
+export default createAppConfig({})
